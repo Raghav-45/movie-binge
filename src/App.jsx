9bde97d619e7ef4dec92ac77d085d903519de442
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2022-07-31 20:16:07 +0530

    Started Implementing Search Method

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import * as React from 'react';
-import { useState } from 'react'
 import './App.css';
 import Login from './Pages/Login'
 import Movie from './Pages/Movie'
-import { MovieThumb } from './Components/MovieComp'
-
-import { Button, SimpleGrid } from '@chakra-ui/react'
 
 function App() {
   return (
-    <Movie></Movie>
+    <Movie />
   );
 }
 
--- src/Pages/Movie.jsx
+++ src/Pages/Movie.jsx
@@ -1,21 +1,43 @@
 import * as React from 'react'
 import { useState } from 'react'
-import { Button, SimpleGrid } from '@chakra-ui/react'
+import { Button, SimpleGrid, InputLeftAddon, Input, InputGroup } from '@chakra-ui/react'
 import { MovieThumb } from '../Components/MovieComp'
 
-export default function Movie() {const [MovieList, setMovieList] = useState([])
+export default function Movie() {
+  
+  const [SearchQuery, setSearchQuery] = useState('')
+  const [MovieList, setMovieList] = useState([])
 
-  async function getmovies(api) {
-    const response = await fetch('https://api.themoviedb.org/3/trending/all/day?api_key=b24785488c1326b9c4442d7325d37724').then(e => e.json())
+  async function getmovies(api='https://api.themoviedb.org/3/trending/all/day?api_key=b24785488c1326b9c4442d7325d37724') {
+    const response = await fetch(api).then(e => e.json())
     setMovieList(response.results)
+    // console.log(response.results)
     return response
   }
-  getmovies()
+
+  async function SearchMovie(str) {
+    if (str) {
+      const response = await fetch('https://api.themoviedb.org/3/search/movie?api_key=b24785488c1326b9c4442d7325d37724&language=en-US&query=' + str + '&page=1&include_adult=false').then(e => e.json())
+      setMovieList(response.results)
+      // console.log(response.results)
+      return response
+    } else {
+      getmovies()
+    }
+  }
+  // getmovies('https://api.themoviedb.org/3/collection/86311?api_key=b24785488c1326b9c4442d7325d37724&language=en-US')
   
   return (
+    <>
+    <InputGroup p={3}>
+      <InputLeftAddon children='Search' />
+      <Input value={SearchQuery} onChange={(e) => {setSearchQuery(e.target.value); SearchMovie(e.target.value)}} placeholder='Query' />
+    </InputGroup>
+    
     <SimpleGrid columns={[2, null, 3]} spacing='30px'>
       {MovieList.map((elem) => <MovieThumb Poster={'https://image.tmdb.org/t/p/original' + elem.poster_path} Name={elem.poster_path} />)}
       <Button onClick={(e) => {console.log(MovieList)}}>Click</Button>
     </SimpleGrid>
+    </>
   );
 }
